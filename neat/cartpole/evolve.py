ff1bbcb1287fa33f5effa4e7c838b19e93a19485
Veemon <russelhughdemos@gmail.com>
2018-01-03 05:55:10 +1000

    updated to newer api

--- neat/cartpole/evolve.py
+++ neat/cartpole/evolve.py
@@ -3,14 +3,9 @@ import os
 import sys
 import random
 
-from math import exp
-
 # third party
 import gym
 from pyneat import pyneat
-
-def distribution(x):
-    return exp(-5 * (x / int(population_size * cutoff)))
 
 def fitness(self):
     # initialize environment
@@ -21,40 +16,46 @@ def fitness(self):
     done = False
     while done == False:
         y = self.network.forward(s)
-        a = y.index(max(y))
+        a = 1 if y[0] > 0 else 0
         s, r, done, _ = env.step(a)
         self.fitness += 1
+
+    return False, self
 
 if __name__ == '__main__':
     # Experiment Params
     input_nodes = 4
-    output_nodes = 2
+    output_nodes = 1
 
     population_size = 1000
-    num_generations = 10
+    num_generations = 100
 
     # Population cutoff percentage
-    cutoff = 0.75
+    cutoff = 0.45
 
     # Percentage chance to mutate
-    mutation = 0.15
+    weight_chance = 0.8
+    structure_chance = 0.4
 
     # Constants used in speciation
-    c1 = 1
-    c2 = 1
-    c3 = 1
+    c1 = 1.0
+    c2 = 1.0
+    c3 = 0.4
+
+    # Speciation threshold
+    sigma_t = 3.0
 
     # Create the gene pool
     gene_pool = pyneat.GenePool(population_size,
                          num_generations,
                          cutoff,
-                         mutation,
                          [c1,c2,c3],
+                         sigma_t,
                          logging=1,
-                         num_threads=4)
+                         num_threads=8)
 
     # Initialise the gene pool
-    gene_pool.init(input_nodes, output_nodes, fitness, distribution)
+    gene_pool.init(input_nodes, output_nodes, fitness)
 
     # Run an evolutionary period
-    gene_pool.evolve()
+    gene_pool.evolve(weight_chance, structure_chance)
